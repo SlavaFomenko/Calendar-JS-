a3d25c84d283b25d65d3fbcd0a48835180e2108a
Slava_Fomenko <mrmagosj@gmail.com>
2023-07-24 14:53:34 +0300

    make chats and display the current date

---
+++ JS/time-block.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const clock = document.getElementById("clock-time")
+const date = document.getElementById("clock-date")
+date.innerText = getFormattedDate();
+
+const timeInterval = setInterval(() => {
+  const time = (new Date()).toLocaleTimeString();
+  clock.innerText = time;
+
+  if(date.innerText != getFormattedDate()){
+    date.innerText = getFormattedDate();фвв
+  }
+}, 1)
+
+function getFormattedDate() {
+  const months = [
+    "января", "февраля", "марта",
+    "апреля", "мая", "июня",
+    "июля", "августа", "сентября",
+    "октября", "ноября", "декабря"
+  ];
+
+  const currentDate = new Date();
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const month = months[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+
+  return(`${day} ${month} ${year} г.`);
+}
